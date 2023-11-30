9a87679ebd3e72808580bd87278f1559c76cee93
Ansh <82747886+Diabloansh@users.noreply.github.com>
2023-12-01 02:28:40 +0530

    Q1.c

---
+++ A4_CS1203_ANSHMADAN/Q1.c
@@ -0,0 +1,84 @@
+//In the code we introduce a printPath function which prints the path from source to each vertex
+//We also alter the printSolution function to accomadate the printpath fnction inside it
+
+
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define V 9
+
+int minDistance(int dist[], bool sptSet[]) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
+}
+
+void printPath(int parent[], int j) {
+    if (parent[j] == -1)
+        return;
+
+    printPath(parent, parent[j]);
+    printf("%d -> ", j);
+}
+
+void printSolution(int dist[], int parent[]) {
+    int src = 0;
+    printf("Vertex \t\t Distance from Source \t Path\n");
+    for (int i = 1; i < V; i++) {
+        printf("%d -> %d \t\t\t %d \t\t\t\t %d -> ", src, i, dist[i], src);
+        printPath(parent, i);
+        printf("\n");
+    }
+}
+
+void dijkstra(int graph[V][V], int src) {
+    int dist[V];
+    bool sptSet[V];
+    int parent[V];
+
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+        parent[i] = -1;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+                parent[v] = u;
+            }
+        }
+    }
+
+    printSolution(dist, parent);
+}
+
+int main() {
+    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                       {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+
+    dijkstra(graph, 0);
+
+    return 0;
+}
+
